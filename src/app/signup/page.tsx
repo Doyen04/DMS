9e386f03f0bcc15ad6fa98db428e98918bbff80b
Doyen04 <solaopeyemi93@gmail.com>
2025-05-29 15:03:32 +0100

    FEAT: update navigation in SignUp and Header components to use Link for better routing

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -3,14 +3,17 @@ import React from "react";
 import Input from "@/app/ui/input";
 import Image from "next/image";
 import Footer from "@/component/footer";
+import Link from "next/link";
 
 
 const SignUp = () => {
     return (
         <div className="w-screen h-screen flex flex-col items-center gap-7.5">
-            <nav className="w-full h-14 px-19 flex items-center gap-1.5 bg-[#e6f0ff] shadow-xs shadow-gray-400">
-                <Image src={'./arrow_back.svg'} width={20} height={20} alt='icon' />
-                <p className="text-center text-[#0c7ff2]">Back to Homepage</p>
+            <nav className="w-full h-14 px-19 flex bg-[#e6f0ff] shadow-xs shadow-gray-400" >
+                <Link className=" flex items-center gap-1.5 " href='/'>
+                    <Image src={'./arrow_back.svg'} width={20} height={20} alt='icon' />
+                    <p className="text-center text-[#0c7ff2]">Back to Homepage</p>
+                </Link>
             </nav>
             <div className="w-[350px] h-[500px] bg-white border-2 border-[#e6f0ff] shadow-lg shadow-gray-500 flex flex-col gap-2 items-center p-2 pt-9 rounded-lg">
                 <div className="text-xl font-extrabold font-inter">Get started for free</div>
@@ -27,7 +30,7 @@ const SignUp = () => {
                     </FormItem>
                     <input className="bg-[#0c7ff2] rounded-sm p-2 text-white" type="button" value="Sign Up" />
                     <p className="text-xs text-center w-full">
-                        By signing up, you agree to our 
+                        By signing up, you agree to our
                         <span className="text-[#0c7ff2] font-medium"> Terms of Service </span>
                         and
                         <span className="text-[#0c7ff2] font-medium"> Privacy Policy </span>
